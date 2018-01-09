94a774ef38a6059b4f3ab59e00e5ba489eaaebec
DESKTOP-68SS1K0\刘金媛 <liujinyuan@agree.com>
2018-01-09 21:13:33 +0800

    播放器初始化

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-unused-vars */
+// import vConsole from 'vconsole'
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
