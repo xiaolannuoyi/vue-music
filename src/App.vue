c24d22c7f1c62e045cc5acc844e4e6e46f72acb1
DESKTOP-68SS1K0\刘金媛 <liujinyuan@agree.com>
2018-01-09 21:47:41 +0800

    header组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    你好
+    <m-header></m-header>
   </div>
 </template>
 
 <script>
+import MHeader from "components/m-header/m-header"
 export default {
-  name: 'app'
+  components: {
+    MHeader,
+  }
 }
 </script>
 
